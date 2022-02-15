b01a391656a03e12a6242272f57d83b492a0569e
nandapark <85476577+park-misun@users.noreply.github.com>
2022-02-15 14:25:27 +0900

    Update variables.tf

--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -9,9 +9,11 @@ variable "address_space" {
 
 variable "rgname" {
     type = string
+    default = "nanda-am-rg"
     description = "Name of resource group"
 }
 variable "location" {
     type = string
+    default = "koreacentral"
     description = "Azure location of storage account environment"
 }
